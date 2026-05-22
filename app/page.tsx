616c8dfa13859d6b54e928e82896e88e1438a093
developerthierry <developerthierry@gmail.com>
2026-05-22 12:37:10 +0000

    Implement Post and PostFeed

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import PostFeed from "@/components/PostFeed";
 import Sidebar from "@/components/Sidebar";
+import Widgets from "@/components/Widgets";
 
 export default function Home() {
   return (    
-    <div className="text-[#0F1419] min-h-screen max-w-[1400px] mx-auto border-2 border-black">
+    <div className="text-[#0F1419] min-h-screen max-w-[1400px] mx-auto border-2 border-black flex">
     <Sidebar/>
-    {/* <PostFeed/>
-    <Widgets/> */}
+    <PostFeed/>
+    <Widgets/>
     </div>
   );
 }
