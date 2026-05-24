a847d29afa8116b5a68927da3215966121dc6025
developerthierry <developerthierry@gmail.com>
2026-05-24 07:25:40 +0000

    Implement Send Post

--- components/PostInput.tsx
+++ components/PostInput.tsx
@@ -1,3 +1,6 @@
+"use client"
+import { db } from "@/firebase";
+import { RootState } from "@/redux/store";
 import {
   CalendarIcon,
   ChartBarIcon,
@@ -5,10 +8,25 @@ import {
   MapPinIcon,
   PhotoIcon,
 } from "@heroicons/react/24/outline";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 const PostInput = () => {
+  const [text, setText] = useState("")
+  const user = useSelector((state:RootState) => state.user)
+  const sendPost = async () => {
+    await addDoc(collection(db, "posts"), {
+        text:text,
+        name:user.name,
+        username:user.username,
+        timestamp:serverTimestamp(),
+        likes:[],
+        comments:[]
+    })
+    setText("")
+  }
   return (
     <div className="flex space-x-5 p-3">
       <Image
@@ -22,6 +40,8 @@ const PostInput = () => {
         <textarea
           className="resize-none outline-none w-full min-h-[50px] text-lg"
           placeholder="What's on your mind?"
+          onChange={(e) => setText(e.target.value)}
+          value={text}
         />
         <div className="flex justify-between pt-5">
           <div className="flex space-x-1.5">
@@ -31,7 +51,10 @@ const PostInput = () => {
             <CalendarIcon className="w-[22px] h-[22px] text-pink-400" />
             <MapPinIcon className="w-[22px] h-[22px] text-pink-400" />
           </div>
-          <button className="bg-pink-400 text-white w-[80px] h-[36px] rounded-full text-sm cursor-pointer">
+          <button className="bg-pink-400 text-white w-[80px] h-[36px] rounded-full text-sm cursor-pointer disabled:bg-opacity-60"
+          onClick={sendPost}
+          disabled={!text}
+          >
             Bubble
           </button>
         </div>
