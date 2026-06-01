0adeaefccd2d12ba312ae1a92491ae8821fbeaf7
developerthierry <developerthierry@gmail.com>
2026-06-01 08:31:22 +0000

    fix issue SignUpPrompt z-index

--- components/SignUpPrompt.tsx
+++ components/SignUpPrompt.tsx
@@ -9,7 +9,7 @@ const SignUpPrompt = () => {
   const name = useSelector((state: RootState) => state.user.name);
   return (
     !name && (
-      <div className="fixed w-full h-[80px] bg-pink-400 bottom-0 flex justify-center items-center md:space-x-5 lg:justify-between lg:px-20 xl:px-40 2xl:px-80">
+      <div className="fixed w-full h-[80px] bg-pink-400 bottom-0 flex justify-center items-center md:space-x-5 lg:justify-between lg:px-20 xl:px-40 2xl:px-80 z-20">
         <div className="hidden md:flex flex-col text-white">
           <span className="text-xl font-bold">
             Stay fly and don't miss out!
